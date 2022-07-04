7923f5e028bed5bd7d7d620905b18e82a26ae82c
Jacob Magnusson <m@jacobian.se>
2022-07-04 16:10:57 +0200

    feat: Initial commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::{
+    env::{args, current_dir},
+    io::{BufRead, Write},
+    process::Command,
+};
+
+fn main() {
+    let filenames_were_passed = !args().skip(1).take(1).count() > 0;
+    if filenames_were_passed {
+        let output = Command::new("cargo")
+            .args(["sqlx", "prepare", "--", "--lib"])
+            .output()
+            .expect("Failed to run command");
+        std::io::stdout().write_all(&output.stdout).unwrap();
+        std::io::stderr().write_all(&output.stdout).unwrap();
+    } else {
+        println!("No files passed in");
+    }
+}
